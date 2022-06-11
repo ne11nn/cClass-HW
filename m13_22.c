a2223c94c78d3f0e05ffff306dd66ac02f9e360e
ne11nn <briiaan.chenn@gmail.com>
2022-06-11 20:13:29 +0800

    homework

--- m13_22.c
+++ m13_22.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <Windows.h>
 
 void main(){ 
 	float a,b,ans;
 
-    printf("Number 1: ");
-    scanf("%f",&a);
-
-    printf("Number 2: ");
-    scanf("%f",&b);
-
-    ans = a + b;
-    printf("%f",ans);
+    printf("Enter in two numbers to add: ");
+    while (scanf("%f%f",&a,&b) == 2)
+    {
+        ans = a + b;
+        printf("%.2f + %.2f = %.2f\n",a,b,ans);
+        Sleep(500);
+        printf("Enter in two numbers to add: ");
+    }
 }
