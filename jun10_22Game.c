6a66c9a4d67402ca53743900aa4f74ede997985f
ne11nn <briiaan.chenn@gmail.com>
2022-06-16 16:38:02 +0800

    homework

--- jun10_22Game.c
+++ jun10_22Game.c
@@ -4,48 +4,120 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void append(char* originalString, char appendingString)
+//decides if the number guessed by player is higher than computer generated number or not
+void higherOrLower(int playerNumber, int goalNumber)
 {
-    int len = strlen(originalString);
-    originalString[len] = appendingString;
-    originalString[len+1] = '\0';
+    if (playerNumber > goalNumber)
+    {
+        printf("My number is lower\n");
+    }
+
+    if (playerNumber < goalNumber)
+    {
+        printf("My number is higher\n");
+    }
 }
 
-void startGame(int number)
+//decides if the number the player guessed is odd or even
+void oddOrEven(int goalNumber)
+{
+    if (goalNumber % 2 == 1)
+    {
+        printf("My number is odd\n");
+    }
+    else
+    {
+        printf("My number is even\n");
+    }
+}
+
+//decides if the number the player guessed is prime or not
+void primeOrNot(int goalNumber)
+{
+    int primeFlag = 0;
+
+    if (goalNumber == 0 || goalNumber == 1) //if the number is 0 or 1 it's prime
+    {
+        primeFlag = 1;
+    }
+
+    for (int i = 2; i <= goalNumber / 2; ++i) //checks if all numbers below the number is a factor of the number or not
+    {
+        if (goalNumber % i == 0) //if it is divisible then set primeflag to 1
+        {
+            primeFlag = 1;
+            break;
+        }
+    }
+
+    if (primeFlag == 0)
+    {
+        printf("My number is prime\n");
+    }
+    if (primeFlag == 1)
+    {
+        printf("My number is not prime\n");
+    }
+}
+
+//function ran when players wins
+void whenPlayerWins()
+{
+    char secondPlayerInput = 'a';
+
+    printf("You got it correct. Nice job!\n");
+    Sleep(300);
+    while (secondPlayerInput != 'y')
+    {
+        if (secondPlayerInput == 'n')
+        {
+            printf("Bye!");
+            Sleep(1500);
+            exit(1);
+        }
+        printf("Do you want to play again?\n(y/n): ");
+        secondPlayerInput = getchar();
+    }
+}
+
+//function ran when player loses
+void whenPlayerLoses(int number)
+{
+    char secondPlayerInput = 'f';
+
+    printf("Nice try but ");
+    Sleep(200);
+    printf("my number was %d\n",number);
+    Sleep(300);
+
+    while (secondPlayerInput != 'y')
+    {
+        if (secondPlayerInput == 'n')
+        {
+            printf("Bye!");
+            Sleep(1500);
+            exit(1);
+        }
+        printf("Do you want to play again?\n(y/n): ");
+        secondPlayerInput = getchar();
+    }
+}
+
+int startGame(int number)
 {
     printf("%d\n",number);
     int playerGuess = -1;
-    char secondPlayerInput = 'f';
     int methodOfHint;
-    int primeFlag = 0;
     int tempNumber = number;
     int sumOfDigits = 0;
     int m = 0;
-    char methodsAlrUsed[] = "6";
-
-    char tmpZero[2] = "0";
-    char tmpOne[2] = "1";
-    char tmpTwo[2] = "2";
-
-    while (tempNumber > 0)
-    {
-        m = tempNumber % 10;
-        sumOfDigits = sumOfDigits + m;
-        tempNumber = tempNumber/10;
-    }
-
-    /*
-        0 = prime/composite
-        1 = odd/even
-        2 = sum of digits
-    */
 
     Sleep(1000);
     printf("I got my number!\n");
     Sleep(800);
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < 4; ++i)
     {
-        printf("%c",methodsAlrUsed);
+        playerGuess = -1;
         while(playerGuess > 100 || playerGuess < 0)
         {
             printf("What's your guess? ");
@@ -55,162 +127,39 @@ void startGame(int number)
 
         if (playerGuess == number)
         {
-            printf("You got it correct. Nice job!\n");
-            Sleep(300);
-            while (secondPlayerInput != 'y')
-            {
-                if (secondPlayerInput == 'n')
-                {
-                    printf("Bye!");
-                    Sleep(1500);
-                    exit(1);
-                }
-                printf("Do you want to play again?");
-                Sleep(300);
-                printf("(y/n): ");
-                scanf("%c",&secondPlayerInput);
-            }
-
+            whenPlayerWins();
             startGame(rand() %101);
-            return;
+            return 0;
         }
 
         if (playerGuess != number)
         {
             printf("Oops! You got it wrong!\n");
             Sleep(400);
-            if (number > playerGuess)
+            if (i != 3)
             {
-                printf("My number is higher\n");
-            }
-            else
-            {
-                printf("My number is lower\n");
+                higherOrLower(playerGuess,number);
             }
             Sleep(400);
-            
-            methodOfHint = 6;
 
-            while(strchr(methodsAlrUsed,methodOfHint) != NULL)
+            if (i == 1)
             {
-                methodOfHint = rand() % 3;
+                oddOrEven(number);
+            }
 
-                if (strchr(methodsAlrUsed,methodOfHint) == NULL)
-                {
-                    if (methodOfHint == 0)
-                    {
-                        if (number == 0 || number == 1)
-                        {
-                            primeFlag = 1;
-                        }
+            if (i == 2)
+            {
+                primeOrNot(number);
+            }
 
-                        for (int i = 2; i <= number / 2; ++i)
-                        {
-                            if (number % i == 0)
-                            {
-                                primeFlag = 1;
-                                break;
-                            }
-                        }
-
-                        if (primeFlag == 0)
-                        {
-                            printf("My number is prime");
-                        }
-                        if (primeFlag == 1)
-                        {
-                            printf("My number is not prime");
-                        }
-
-                       strcat(methodsAlrUsed,tmpZero);
-                    }
-
-                    if (methodOfHint == 1)
-                    {
-                        if (number % 2 == 1)
-                        {
-                            printf("My number is odd");
-                        }
-                        else
-                        {
-                            printf("My number is even");
-                        }
-                        
-                        strcat(methodsAlrUsed,tmpOne);
-                    }
-
-                    if (methodOfHint == 2)
-                    {
-                        printf("The sum of my digits is %d",sumOfDigits);
-                        
-
-                        strcat(methodsAlrUsed,tmpTwo);
-                    }
-                }
+            if (i == 3)
+            {
+                whenPlayerLoses(number);
+                startGame(rand() %101);
+                return 0;
             }
         }
-
-        playerGuess = -1;
-        secondPlayerInput = 'f';
-        primeFlag = 0;
-        tempNumber = number;
-        sumOfDigits = 0;
-        m = 0;
-    }
-
-    while(playerGuess > 100 || playerGuess < 0)
-    {
-        printf("What's your guess? ");
-        scanf("%d",&playerGuess);
-        Sleep(750);
-    }
-
-    if (playerGuess == number)
-    {
-        printf("You got it correct. Nice job!\n");
-        Sleep(300);
-        while (secondPlayerInput != 'y')
-        {
-            if (secondPlayerInput == 'n')
-            {
-                printf("Bye!");
-                Sleep(1500);
-                exit(1);
-            }
-            printf("Do you want to play again?");
-            Sleep(300);
-            printf("(y/n): ");
-            scanf("%c",&secondPlayerInput);
-        }
-
-        startGame(rand() %101);
-        return;
-    }
-
-    if (playerGuess != number)
-    {
-        printf("Oops! You got it wrong!\n");
-        Sleep(500);
-        printf("My number was %d\n",number);
-        Sleep(500);
-        while (secondPlayerInput != 'y')
-        {
-            if (secondPlayerInput == 'n')
-            {
-                printf("Bye!");
-                Sleep(1500);
-                exit(1);
-            }
-            printf("Would you like to try again?");
-            Sleep(300);
-            printf("(y/n): ");
-            scanf("%c",&secondPlayerInput);
-        }
-
-        startGame(rand() %101);
-        return;
-    }
-    
+    } 
 }
 
 void main()
@@ -224,7 +173,7 @@ void main()
         {
             exit(1);
         }
-        printf("Welcome to a number guessing game. \nI will choose a random number from 0-100\nand you will have to guess the number. \nI will also give hints if you get them wrong. \nIf you are up to the challenge, type in y. If not then type in n. \nGood luck!\n(y/n): ");
+        printf("Welcome to a number guessing game. \nI will choose a random number from 0-100\nand you will have to guess the number in 4 tries. \nI will also give hints if you get them wrong. \nIf you are up to the challenge, type in y. If not then type in n. \nGood luck!\n(y/n): ");
         scanf("%c",&beginningInput);
     }
 
